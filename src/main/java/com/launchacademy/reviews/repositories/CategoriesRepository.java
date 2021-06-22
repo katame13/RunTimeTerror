3bcbd37294f438325e337bae7390637542a80c8c
Christian <43299063+Christiandecolombia@users.noreply.github.com>
2021-06-22 09:17:56 -0700

    Created categories model, repository, seeder, main seeder, service

---
+++ src/main/java/com/launchacademy/reviews/repositories/CategoriesRepository.java
@@ -0,0 +1,12 @@
+package com.launchacademy.reviews.repositories;
+
+import com.launchacademy.reviews.models.Category;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriesRepository extends CrudRepository<Category, Integer> {
+
+  List<Category> findByName(String name);
+}
---
+++ src/main/java/com/launchacademy/reviews/services/CategoriesService.java
@@ -0,0 +1,37 @@
+package com.launchacademy.reviews.services;
+
+import com.launchacademy.reviews.models.Category;
+import com.launchacademy.reviews.repositories.CategoriesRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriesService {
+
+  private CategoriesRepository categoriesRepository;
+
+  @Autowired
+  public CategoriesService(
+      CategoriesRepository categoriesRepository) {
+    this.categoriesRepository = categoriesRepository;
+  }
+
+  public void save(Category category) {
+    categoriesRepository.save(category);
+  }
+
+  public List<Category> findByName(String name) {
+    return categoriesRepository.findByName(name);
+  }
+
+  public List<Category> findAll() {
+    return (List<Category>) categoriesRepository.findAll();
+  }
+
+  public Category findById(Integer id) {
+    return categoriesRepository.findById(id).get();
+  }
+
+}
