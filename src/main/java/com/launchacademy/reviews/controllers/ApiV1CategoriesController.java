03a3708ad4a4da9f6b5c02e4e9a4e3cb0c075eef
katame13 <70725696+katame13@users.noreply.github.com>
2021-06-24 14:33:58 -0700

    Update ApiV1CategoriesController.java
    working on updating the categories contoller

--- src/main/java/com/launchacademy/reviews/controllers/ApiV1CategoriesController.java
+++ src/main/java/com/launchacademy/reviews/controllers/ApiV1CategoriesController.java
@@ -5,10 +5,12 @@ import com.launchacademy.reviews.services.CategoriesService;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/categories")
@@ -25,5 +27,32 @@ public class ApiV1CategoriesController {
     Map<String, List<Category>> categories= new HashMap<>();
     categories.put("categories", categoriesService.findAll());
     return categories;
+  }
+
+  @GetMapping("/{id}")
+  public Map<String, Category> getCategory(@PathVariable int id) {
+    Optional<Category> category = categoriesService.optionalFindById(id);
+    if(category.isEmpty()){
+      throw new CategoryNotFoundException();
+    }else{
+
+      return
+    }
+  }
+
+  @NoArgsConstructor
+  private class CategoryNotFoundException extends RuntimeException {
+
+  }
+
+  @ControllerAdvice
+  private class CategoryNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(ApiV1CategoriesController.CategoryNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String CategoryNotFoundHandler(ApiV1CategoriesController.CategoryNotFoundException ex) {
+      return ex.getMessage();
+    }
   }
 }
