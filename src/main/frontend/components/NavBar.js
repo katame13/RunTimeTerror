e01578876125b69dbbaf89cfa11f90ddd82f82dc
katame13 <70725696+katame13@users.noreply.github.com>
2021-07-01 14:52:53 -0700

    Starting to style the Stars for Rating
    took out underlines for links in nav bar

--- src/main/frontend/components/NavBar.js
+++ src/main/frontend/components/NavBar.js
@@ -32,12 +32,12 @@ const NavBar = props => {
   const categoryLinks = categories.map(category => {
     return (
 
-        <Link key={category.id} to={`/categories/${category.id}`}>{category.name}</Link>
+        <Link key={category.id} to={`/categories/${category.id}`} style={{textDecoration: 'none'}}>{category.name}</Link>
     )
   })
 
   categoryLinks.unshift(
-      <Link key={0} to={`/sites`}>Home</Link>
+      <Link key={0} to={`/sites`} style={{textDecoration: 'none'}}>Home</Link>
   )
 
   return (
