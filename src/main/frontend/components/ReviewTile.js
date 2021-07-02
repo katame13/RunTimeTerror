b2b334bc30555833853e18bb29b634e6d3cc7b97
Christian <43299063+Christiandecolombia@users.noreply.github.com>
2021-07-02 09:54:53 -0700

    finished imported star rating

--- src/main/frontend/components/ReviewTile.js
+++ src/main/frontend/components/ReviewTile.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import StarRatingComponent from "react-star-rating-component";
+
 
 const ReviewTile = (props) => {
   const { userName, review, longestRelationship, rating } = props.review;
@@ -6,7 +8,13 @@ const ReviewTile = (props) => {
     <div>
       <h4>Reviews</h4>
       <p>User Name: {userName}</p>
-      <p>Rating: {rating}</p>
+      <div>Rating:
+      <StarRatingComponent
+        name="rate2"
+        editing={false}
+        starCount={5}
+        value={rating}
+      /></div>
       <p>Longest Relationship: {longestRelationship}</p>
       <p>Review: {review}</p>
     </div>
--- src/main/frontend/components/SiteTile.js
+++ src/main/frontend/components/SiteTile.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import StarRatingComponent from 'react-star-rating-component';
 
 const SiteTile = (props) => {
   const {id, name, url, imgUrl, category, description, reviews} = props.site
-
   let reviewsLength = reviews.length
   let sumRating = 0
   let sumRelationship = 0
@@ -23,7 +23,14 @@ const SiteTile = (props) => {
           <img src={imgUrl}></img>
           <h2>{name}</h2>
         </Link>
-        <h4>Average Rating: {aveRating.toPrecision(3)}</h4>
+        {/*<h4>Average Rating: {aveRating.toPrecision(3)}</h4>*/}
+        <h4>Average Rating:
+      <StarRatingComponent
+        name="rate2"
+        editing={false}
+        starCount={5}
+        value={aveRating}
+      /></h4>
         <h5>Average Relationship Length: {aveRelationship.toPrecision(
           3)} weeks</h5>
     </div>
