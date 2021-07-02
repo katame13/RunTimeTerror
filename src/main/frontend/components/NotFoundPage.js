4f900f8225a6a5fb75838bbbecfa03574afe9a48
katame13 <70725696+katame13@users.noreply.github.com>
2021-07-02 11:31:35 -0700

    a few tweeks
    Added in main
    updated font for consistancy

--- src/main/frontend/components/NotFoundPage.js
+++ src/main/frontend/components/NotFoundPage.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const NotFoundPage=()=>(
-<div>
-  <h1>Page Not Found</h1>
+<div className="site-tile-container-header">
+  <h1>Love Not Found</h1>
   <img src={"https://error404.fun/img/full-preview/1x/9.png"} height="100%" alt="Page Not Found" />
 </div>
 
